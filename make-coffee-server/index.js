6a45f1a699658eb81dd5d658707058f798a22f02
arif1101 <arifurrahmanarif223@gmail.com>
2025-01-05 11:47:13 +0600

    insert, read, delete data coffee

--- make-coffee-server/index.js
+++ make-coffee-server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,6 +22,32 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
+    const coffeeCollection = client.db('coffee').collection('coffee');
+
+    app.get('/coffee', async(req,res)=>{
+      const cursor=coffeeCollection.find()
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+    
+    // add data
+    app.post('/coffee', async(req, res)=>{
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result)
+    })
+
+    //delete data
+    app.delete('/coffee/:id',async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeeCollection.deleteOne(query)
+      res.send(result)
+    })
+    
+    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
