c156615f2a9517c3f0d332273073363b70e81b97
arif1101 <arifurrahmanarif223@gmail.com>
2025-01-05 11:48:57 +0600

    card design and fetch delete data coffee

--- make-coffee-client/src/App.jsx
+++ make-coffee-client/src/App.jsx
@@ -7,15 +7,16 @@ function App() {
   console.log('coffes from app : ',coffes)
   return (
     <>
-      <div>
+      <div className="max-w-[1320px] mx-auto">
       <h1 className="text-3xl font-bold">
       our all coffee count : {coffes.length}
       </h1>
 
-
+      <div className="grid grid-cols-2 gap-6">
       {
         coffes.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
       }
+      </div>
       
       </div>
     </>
